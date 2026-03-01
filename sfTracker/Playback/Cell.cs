7ac07d51e0830662099f1436e28c691d277dfb5f
sutson <aberdeenrules@hotmail.com>
2026-03-01 15:59:09 +0000

    update Cell to have bank number

--- sfTracker/Playback/Cell.cs
+++ sfTracker/Playback/Cell.cs
@@ -5,8 +5,9 @@
     /// </summary>
     public class Cell
     {
-        public int Channel { get; set; } = -1;   // column corresponding to cell
-        public int Note { get; set; } = -1;      // pitch of note
+        public int Channel { get; set; } = -1;    // column corresponding to cell
+        public int Note { get; set; } = -1;       // pitch of note
+        public int Bank { get; set; } = -1;       // bank number in SoundFont
         public int Instrument { get; set; } = -1; // instrument number in SoundFont
         public int Velocity { get; set; } = 100;  // volume of the cell
     }
