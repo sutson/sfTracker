f2f5df795f12fece7226ae740ed572e7bb1b1a25
sutson <aberdeenrules@hotmail.com>
2026-03-01 15:56:55 +0000

    add keybind enums, currently just keyboard to note mapping

---
+++ sfTracker/Controls/KeyboardToMidiNote.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace sfTracker.Controls
+{
+    public static class KeyboardToMidiNote
+    {
+        public static readonly Dictionary<Key, MidiNoteValueMap> Map =
+            new Dictionary<Key, MidiNoteValueMap>
+            { 
+                { Key.Z, MidiNoteValueMap.C_3 },
+                { Key.S, MidiNoteValueMap.C_SHARP_3 },
+                { Key.X, MidiNoteValueMap.D_3 },
+                { Key.D, MidiNoteValueMap.D_SHARP_3 },
+                { Key.C, MidiNoteValueMap.E_3 },
+                { Key.V, MidiNoteValueMap.F_3 },
+                { Key.G, MidiNoteValueMap.F_SHARP_3 },
+                { Key.B, MidiNoteValueMap.G_3 },
+                { Key.H, MidiNoteValueMap.G_SHARP_3 },
+                { Key.N, MidiNoteValueMap.A_3 },
+                { Key.J, MidiNoteValueMap.A_SHARP_3 },
+                { Key.M, MidiNoteValueMap.B_3 },
+                { Key.OemComma, MidiNoteValueMap.C_4 }, // comma key
+                { Key.L, MidiNoteValueMap.C_SHARP_4 },
+                { Key.OemPeriod, MidiNoteValueMap.D_4 }, // full stop key
+
+                { Key.Q, MidiNoteValueMap.C_4 },
+                { Key.D2, MidiNoteValueMap.C_SHARP_4 },
+                { Key.W, MidiNoteValueMap.D_4 },
+                { Key.D3, MidiNoteValueMap.D_SHARP_4 },
+                { Key.E, MidiNoteValueMap.E_4 },
+                { Key.R, MidiNoteValueMap.F_4 },
+                { Key.D5, MidiNoteValueMap.F_SHARP_4 },
+                { Key.T, MidiNoteValueMap.G_4 },
+                { Key.D6, MidiNoteValueMap.G_SHARP_4 },
+                { Key.Y, MidiNoteValueMap.A_4 },
+                { Key.D7, MidiNoteValueMap.A_SHARP_4 },
+                { Key.U, MidiNoteValueMap.B_4 },
+                { Key.I, MidiNoteValueMap.C_5 },
+                { Key.D9, MidiNoteValueMap.C_SHARP_5 },
+                { Key.O, MidiNoteValueMap.D_5 },
+            };
+    }
+
+}
---
+++ sfTracker/Controls/MidiNoteValueMap.cs
@@ -0,0 +1,33 @@
+﻿namespace sfTracker.Controls
+{
+    public enum MidiNoteValueMap
+    {
+        C_3         = 48,
+        C_SHARP_3   = 49,
+        D_3         = 50,
+        D_SHARP_3   = 51,
+        E_3         = 52,
+        F_3         = 53,
+        F_SHARP_3   = 54,
+        G_3         = 55,
+        G_SHARP_3   = 56,
+        A_3         = 57,
+        A_SHARP_3   = 58,
+        B_3         = 59,
+        C_4         = 60,
+        C_SHARP_4   = 61,
+        D_4         = 62,
+        D_SHARP_4   = 63,
+        E_4         = 64,
+        F_4         = 65,
+        F_SHARP_4   = 66,
+        G_4         = 67,
+        G_SHARP_4   = 68,
+        A_4         = 69,
+        A_SHARP_4   = 70,
+        B_4         = 71,
+        C_5         = 72,
+        C_SHARP_5   = 73,
+        D_5         = 74
+    }
+}
