46d92672c7386758e47c29b607e73e8f4e534624
sutson <aberdeenrules@hotmail.com>
2026-03-01 16:00:59 +0000

    add soundfont preset class for selected presets

---
+++ sfTracker/Tracker/SoundFontPreset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sfTracker.Tracker
+{
+    public class SoundFontPreset
+    {
+        public string Name { get; set; }
+        public int DisplayID { get; set; }
+        public int Bank { get; set; }
+        public int Instrument { get; set; }
+
+        public string Display => $"{DisplayID}: {Name}";
+    }
+}
