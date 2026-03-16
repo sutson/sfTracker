198556cdd494ef18e84ca994b0a3efd621aa6cce
sutson <aberdeenrules@hotmail.com>
2026-03-16 01:18:01 +0000

    added comments to playback classes

--- sfTracker/Playback/Cell.cs
+++ sfTracker/Playback/Cell.cs
@@ -7,12 +7,12 @@ namespace sfTracker.Playback
     /// </summary>
     public class Cell
     {
-        public int Channel { get; set; } = -1;    // column corresponding to cell
-        public int Note { get; set; } = -1;       // pitch of note
-        public int Bank { get; set; } = -1;       // bank number in SoundFont
-        public int Instrument { get; set; } = -1; // instrument number in SoundFont
+        public int Channel { get; set; } = -1;      // column corresponding to cell
+        public int Note { get; set; } = -1;         // pitch of note
+        public int Bank { get; set; } = -1;         // bank number in SoundFont
+        public int Instrument { get; set; } = -1;   // instrument number in SoundFont
         public int InstrumentID { get; set; } = -1; // instrument ID for displaying on the tracker grid
-        public int Velocity { get; set; } = -1;  // volume of the cell
-        public PanEffect Panning { get; set; } = ProgramConstants.DefaultPanEffect;  // panning of the cell
+        public int Velocity { get; set; } = -1;     // volume of the cell
+        public PanEffect Panning { get; set; } = ProgramConstants.DefaultPanEffect ;  // panning of the cell
     }
 }
--- sfTracker/Playback/PanEffect.cs
+++ sfTracker/Playback/PanEffect.cs
@@ -2,6 +2,9 @@
 
 namespace sfTracker.Playback
 {
+    /// <summary>
+    /// Class for facilitating <c>Cell</c> note panning.
+    /// </summary>
     public class PanEffect(EffectType? direction, int value)
     {
         public EffectType? Direction { get; set; } = direction;
