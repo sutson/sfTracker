88deee60515d375a137352e74d301295072c8b84
sutson <aberdeenrules@hotmail.com>
2026-03-16 01:15:12 +0000

    added comments to helpers

--- sfTracker/Helpers/ProjectFile.cs
+++ sfTracker/Helpers/ProjectFile.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace sfTracker.Helpers
 {
+    /// <summary>
+    /// Class for creating the structure of a project for saving to file.
+    /// </summary>
     public class ProjectFile
     {
         public string ProjectName { get; set; }
